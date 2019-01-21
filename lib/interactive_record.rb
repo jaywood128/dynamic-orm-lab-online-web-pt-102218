1712f3d874d0e37129af7810d343e3a4223d5ff0
Johnathon Wood <jaywood128@gmail.com>
2019-01-21 19:55:33 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -65,11 +65,15 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
-
-    attribute.keys.join = attribute_key
-    attribute.values.join = attribute_value
-    sql = "SELECT * FROM #{self.table_name} WHERE #{atttribute_key} = #{attribute.values}"
-    DB[:conn].execute(sql)
+    attribute_key = attribute.keys.join()
+    attrubute_value = attribute.values.first
+    sql =<<-SQL
+      SELECT * FROM #{self.table_name}
+      WHERE #{attribute_key} = "#{attrubute_value}"
+      LIMIT 1
+    SQL
+    row = DB[:conn].execute(sql)
+    
   end
 
 end
