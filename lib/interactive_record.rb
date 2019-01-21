4ee71b1217cedf4f44b16024418129e63d155f0d
Johnathon Wood <jaywood128@gmail.com>
2019-01-21 17:50:51 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -16,7 +16,7 @@ class InteractiveRecord
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |column|
-      
+
       column_names << column["name"]
     end
     column_names.compact
