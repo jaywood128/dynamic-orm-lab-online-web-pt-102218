9950888523f71d01100a975ef8705194a56aa1b6
Johnathon Wood <jaywood128@gmail.com>
2019-01-21 19:06:55 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -37,7 +37,7 @@ class InteractiveRecord
     self.class.table_name
   end
 
-  def col_names_for_insert 
+  def col_names_for_insert
     self.class.column_names.delete_if {|col| col.name == "id"}.join(',')
   end
 
